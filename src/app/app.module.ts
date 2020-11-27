6521e382dc5791987638da0cbd6c6e1be2de69b0
Deian Petrov <deian.petrov@sap.com>
2020-11-27 15:27:19 +0200

    Styles & main & welcome & nav & routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './shared/welcome/welcome.component';
+import { NotfoundComponent } from './shared/notfound/notfound.component';
+
+const routes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'notFound', component: NotfoundComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: '**', redirectTo: 'notFound', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    NotfoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
