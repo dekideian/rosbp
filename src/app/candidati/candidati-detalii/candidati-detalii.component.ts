e65f709f5ed6d1ec837185a6100186112fbcd7ea
Deian Petrov <deian.petrov@sap.com>
2020-12-07 22:30:46 +0200

    Praised be the Lord

--- src/app/candidati/candidati-detalii/candidati-detalii.component.ts
+++ src/app/candidati/candidati-detalii/candidati-detalii.component.ts
@@ -6,7 +6,7 @@ import { SalariatiService } from '../candidati.service';
 import { FilesService } from '../../services/files.service';
 import { FileDetails, TemplateDetails } from 'src/app/shared/upload-file/uploadedFileDetails';
 import { AuthService } from 'src/app/services/auth.service';
-import { FirmeService } from 'src/app/firme/firme.service';
+import { TemplateHandler } from 'easy-template-x';
 @Component({
   selector: 'app-candidati-detalii',
   templateUrl: './candidati-detalii.component.html',
@@ -64,8 +64,51 @@ export class CandidatiDetaliiComponent implements OnInit {
   goBack() {
     this.router.navigate(['/candidati']);
   }
+  async aici() {
+    console.log('hah');
+    const data = {
+      posts: [
+          {
+            nrContract: 'Numar Contract',
+            dataContract: 'Data Contract'
+          }]
+      };
+    const response = await fetch(this.templateDetails.documentUrl);
+    const templateFile = await response.blob();
+    const handler = new TemplateHandler();
+    const doc = await handler.process(templateFile, data);
+    this.saveFile('CIM_' + this.templateDetails.nume, doc);
+
+  }
+
+
 
   deleteFile(fileId: string, documentURL: string) {
     this.filesService.removeFile(fileId, documentURL);
   }
+
+  saveFile(filename, blob) {
+
+    // see: https://stackoverflow.com/questions/19327749/javascript-blob-filename-without-link
+
+    // get downloadable url from the blob
+    const blobUrl = URL.createObjectURL(blob);
+
+    // create temp link element
+    let link = document.createElement("a");
+    link.download = filename;
+    link.href = blobUrl;
+
+    // use the link to invoke a download
+    document.body.appendChild(link);
+    link.click();
+
+    // remove the link
+    setTimeout(() => {
+        link.remove();
+        window.URL.revokeObjectURL(blobUrl);
+        link = null;
+    }, 0);
+}
+
 }
