761d4e0165a49ae27ef376bf3b498670205ce310
Deian Petrov <deian.petrov@sap.com>
2020-12-08 17:57:29 +0200

    Add Export to csv

--- src/app/candidati/candidati-detalii/candidati-detalii.component.ts
+++ src/app/candidati/candidati-detalii/candidati-detalii.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { Candidat, ICandidatLocal } from '../candidat';
+import { Candidat, CandidatCSV, ICandidatLocal } from '../candidat';
 import { SalariatiService } from '../candidati.service';
 import { FilesService } from '../../services/files.service';
 import { FileDetails, TemplateDetails } from 'src/app/shared/upload-file/uploadedFileDetails';
 import { AuthService } from 'src/app/services/auth.service';
 import { TemplateHandler } from 'easy-template-x';
+import * as fileSaver from 'file-saver';
+
 @Component({
   selector: 'app-candidati-detalii',
   templateUrl: './candidati-detalii.component.html',
@@ -95,7 +97,7 @@ export class CandidatiDetaliiComponent implements OnInit {
     const blobUrl = URL.createObjectURL(blob);
 
     // create temp link element
-    let link = document.createElement("a");
+    let link = document.createElement('a');
     link.download = filename;
     link.href = blobUrl;
 
@@ -110,5 +112,91 @@ export class CandidatiDetaliiComponent implements OnInit {
         link = null;
     }, 0);
 }
+
+  csvExport() {
+
+    const data: CandidatCSV[] = [{
+      nrContract: this.salariat.nrContract,
+      dataContract: this.salariat.dataContract,
+      numeSalariat: this.salariat.numeSalariat,
+      prenumeSalariat: this.salariat.prenumeSalariat,
+      marca: this.salariat.marca,
+      tara: this.salariat.tara,
+      judet: this.salariat.judet,
+      localitate: this.salariat.localitate,
+      strada: this.salariat.strada,
+      numar: this.salariat.numeSalariat,
+      bloc: this.salariat.bloc,
+      scara: this.salariat.scara,
+      etaj: this.salariat.etaj,
+      apartament: this.salariat.apartament,
+      actIdentitate: this.salariat.actIdentitate,
+      serieCI: this.salariat.serieCI,
+      numarCI: this.salariat.numarCI,
+      unitateaCareAEliberatCI: this.salariat.unitateaCareAEliberatCI,
+      dataEliberareCI: this.salariat.dataEliberareCI,
+      dataExpirareCI: this.salariat.dataExpirareCI,
+      cnp: this.salariat.cnp,
+      dataAngajare: this.salariat.dataAngajare,
+      dataAngajareNedeterminat: this.salariat.dataAngajareNedeterminat,
+      nrLuniSaptamaniAni: this.salariat.nrLuniSaptamaniAni,
+      dataInceputCimDeteriminat: this.salariat.dataInceputCimDeteriminat,
+      dataSfarsitCimDeterminat: this.salariat.dataSfarsitCimDeterminat,
+      departament: this.salariat.departament,
+      locDeMunca: this.salariat.locDeMunca,
+      functia: this.salariat.functia,
+      codCOR: this.salariat.codCOR,
+      normaIntreagaDeLucruOreZi: this.salariat.normaIntreagaDeLucruOreZi,
+      normaIntreagaDeLucruOreSapt: this.salariat.normaIntreagaDeLucruOreSapt,
+      normaPartiala: this.salariat.normaPartiala,
+      repartizareProgramPtNormaPartiala: this.salariat.repartizareProgramPtNormaPartiala,
+      repartizareTimpMunca: this.salariat.repartizareTimpMunca,
+      tipIntervalRepartizare: this.salariat.tipIntervalRepartizare,
+      durataConcediuDeOdihna: this.salariat.durataConcediuDeOdihna,
+      salariulDeBazaBrut: this.salariat.salariulDeBazaBrut,
+      perioadaDeProba: this.salariat.perioadaDeProba,
+      perioadaDePreavizInCazulConcedierii: this.salariat.perioadaDePreavizInCazulConcedierii,
+      perioadaDePreavizInCazulDemisiei: this.salariat.perioadaDePreavizInCazulDemisiei,
+      anulCurent: this.salariat.anulCurent,
+      nrInregCerereDeAngajare: this.salariat.nrInregCerereDeAngajare,
+      nrInregDeclaratieFunctieDeBaza: this.salariat.nrInregDeclaratieFunctieDeBaza,
+      nrInregDeclaratiePersoaneInIntretinere: this.salariat.nrInregDeclaratiePersoaneInIntretinere,
+      nrInregDeclaratieCasaDeSanatate: this.salariat.nrInregDeclaratieCasaDeSanatate,
+      nrInregDeclLuareLaCunostintaROI: this.salariat.nrInregDeclLuareLaCunostintaROI,
+      nrInregPlanificareaZilelorDeCO: this.salariat.nrInregPlanificareaZilelorDeCO,
+      nrZileCOConveniteInAnulCurent: this.salariat.nrZileCOConveniteInAnulCurent,
+      platitorDeImpozit: this.salariat.platitorDeImpozit,
+      functiaDeBaza: this.salariat.functiaDeBaza,
+      mail: this.salariat.mail,
+      parolaWeb: this.salariat.parolaWeb,
+      locatiePlata: this.salariat.locatiePlata,
+      bancaAngajator: this.salariat.bancaAngajator,
+      iban: this.salariat.iban,
+      tipContract: this.salariat.tipContract,
+      sablonContractNexus: this.salariat.sablonContractNexus,
+      angajatorNexus: this.salariat.angajatorNexus,
+      cuiAngajator: this.salariat.cuiAngajator,
+      cuiLocDeMunca: this.salariat.cuiLocDeMunca,
+      ticheteDeMasa: this.salariat.ticheteDeMasa,
+      studiiSCED: this.salariat.studiiSCED
+    }];
+    //  const data = data2.items;
+
+    const replacer = (key, value) => value === null ? '' : value; // specify how you want to handle null values here
+    const header = Object.keys(data[0]);
+    const csv = data.map(row => {
+         return header.map(fieldName => {
+            return fieldName + ';' + JSON.stringify(row[fieldName], replacer);
+          }).join('\r\n');
+      }
+      );
+
+    const tableHeaders = ['Denumire camp', 'Valoare'];
+    csv.unshift(tableHeaders.join(';'));
+    const csvArray = csv.join('\r\n');
+
+    const blob = new Blob([csvArray], {type: 'text/csv' });
+    this.saveFile('myFile.csv', blob);
+  }
 
 }
